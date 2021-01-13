44930b5a56dc2d5905560856e438cb84a6212ec0
nardo88 <nardo1988@mail.ru>
2021-01-13 11:58:06 +0300

    open close popup with class popup-repair-types

--- src/modules/headerMenu.js
+++ src/modules/headerMenu.js
@@ -1,19 +1,59 @@
 const headerMenu = () => {
-   const menuIcon = document.querySelector('.menu__icon');
    const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+   const popupRepairTypes = document.querySelector('.popup-repair-types');
 
-    // класс для меню popup-dialog-menu--active
+  
     const toggleMenu = () => {
+        popupDialogMenu.parentElement.classList.toggle('visible')
         popupDialogMenu.classList.toggle('popup-dialog-menu--active');
     }
 
-    menuIcon.addEventListener('click', toggleMenu);
+    const openPopupRepairTypes = () => {
+        popupRepairTypes.classList.add('visible');
 
-    popupDialogMenu.addEventListener('click', e => {
+    }
+
+    const closePopupRepairTypes = () => {
+        popupRepairTypes.classList.remove('visible');
+
+    }
+
+
+    document.addEventListener('click', e => {
+        e.preventDefault();
         const target = e.target;
-
+        // закрываем меню
         if (target.closest('.close-menu')) {
             toggleMenu();
+        }
+        // открываем меню
+        if (target.closest('.menu__icon')) {
+            toggleMenu();
+        }
+        // скролл до нужной позиции
+        if (target.closest('.popup-menu-nav__item')){
+            const scrollHeight = document.getElementById(target.href.split('#')[1]).offsetTop;
+            toggleMenu();
+            window.scrollTo({top: scrollHeight, behavior: 'smooth'});
+        }
+        // открытие модалки по клику виды ремонта
+        if (target.parentElement.matches('.link-list-repair')){
+            openPopupRepairTypes()
+        }
+        // открытие модалки по клику в меню
+        if (target.parentElement.matches('.link-list-menu')) {
+            toggleMenu();
+            openPopupRepairTypes()
+        }
+        
+    });
+    // работа с модалкой popupRepairTypes
+    popupRepairTypes.addEventListener('click', e => {
+        e.preventDefault();
+        const target = e.target;
+        // закрытие модалки
+        if (target.closest('.close')) {
+            closePopupRepairTypes()
         }
     });
 
