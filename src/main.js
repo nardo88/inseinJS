85183a03ef90a8846523beafe0761cabc312a6a7
nardo88 <nardo1988@mail.ru>
2021-01-13 10:23:21 +0300

    create toggle menu

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import phoneNumbers from './modules/phoneNumbers';
+import headerMenu from './modules/headerMenu';
 
-
+// отображение доп. телефона
 phoneNumbers();
+// отображение меню
+headerMenu();
---
+++ src/modules/headerMenu.js
@@ -0,0 +1,34 @@
+const headerMenu = () => {
+   const menuIcon = document.querySelector('.menu__icon');
+   const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+
+    // класс для меню popup-dialog-menu--active
+    const toggleMenu = () => {
+        popupDialogMenu.classList.toggle('popup-dialog-menu--active');
+    }
+
+
+    menuIcon.addEventListener('click', toggleMenu);
+
+    popupDialogMenu.addEventListener('click', e => {
+        const target = e.target;
+
+        if (target.closest('.close-menu')) {
+            toggleMenu();
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+export default headerMenu;
