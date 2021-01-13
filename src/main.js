da79b44b63a20631b6af57c6f3f74639591e787b
nardo88 <nardo1988@mail.ru>
2021-01-13 12:41:25 +0300

    mask Phones

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import phoneNumbers from './modules/phoneNumbers';
 import headerMenu from './modules/headerMenu';
+import maskForPhones from './modules/maskForPhones';
 
 // отображение доп. телефона
 phoneNumbers();
 // отображение меню
 headerMenu();
+// маска для телефона
+maskForPhones();
